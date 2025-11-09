fe2769713e4c4f151b74985b486f4ad379365396
jieun0824 <suspen064@gmail.com>
2025-11-09 17:21:22 +0900

    fix: articleList template 방식에서 NodeElement 로 변경

--- pages/ArticlesList/js/createListItem.js
+++ pages/ArticlesList/js/createListItem.js
@@ -1,41 +1,30 @@
 import { PATHS } from "../../../shared/constants/paths.js";
+import { ListItemComponent } from "../components/ListItemComponent.js";
 
-// const dummyImg = "https://dummyimage.com/300";
-
-export function createListItem(article, templateId) {
-  const template = document.getElementById(templateId).content.cloneNode(true);
-  const templateTitle = template.querySelector(".list-item-title");
-  const templateHeader = template.querySelector(".list-item-header");
-  const metas = template.querySelectorAll(".list-item-meta > div");
-
-  const footer = template.querySelector(".list-item-footer");
-  // const footerAvatar = template.querySelector(".list-item-avatar");
-  const footerUser = template.querySelector(".list-item-user");
-
+export function createListItem(article) {
   const { articleId, title: postTitle, createdAt, author, status } = article;
   const { id: authorId, nickname, profileImageUrl } = author;
   const { likes, comments, views } = status;
 
-  const rootElement = template.firstElementChild;
-  rootElement.id = `article-${articleId}`;
+  const node = ListItemComponent({
+    articleId,
+    postTitle,
+    likes,
+    comments,
+    views,
+    profileImageUrl,
+    createdAt,
+    authorId,
+    nickname,
+  });
 
-  template.id = articleId;
-  templateTitle.textContent = postTitle;
+  const dom = node.getDom();
 
-  metas[0].textContent = `좋아요 ${likes} 댓글 ${comments} 조회수 ${views}`;
-  metas[1].textContent = createdAt;
-
-  footer.id = `author-${authorId}`;
-  footerUser.textContent = nickname;
-
-  templateHeader.addEventListener("click", (e) => {
+  const listItemHeader = dom.querySelector(".list-item-header");
+  listItemHeader.addEventListener("click", (e) => {
     e.stopPropagation();
     window.location.href = PATHS.ARTICLE_DETAIL.ABSOLUTE(articleId);
   });
 
-  //TODO: 이미지 넣기
-
-  footerUser.textContent = nickname;
-
-  return template;
+  return dom;
 }
