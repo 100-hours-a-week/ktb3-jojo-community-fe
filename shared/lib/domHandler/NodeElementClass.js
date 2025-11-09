0a1c4609e007406038ef885b50bba568acc9b534
jieun0824 <suspen064@gmail.com>
2025-11-09 23:30:14 +0900

    feat: 댓글 수정 삭제 기능 연결

--- shared/lib/domHandler/NodeElementClass.js
+++ shared/lib/domHandler/NodeElementClass.js
@@ -6,9 +6,44 @@ function createDom(context) {
 
 class NodeElementClass {
   #dom;
+  #eventListeners = [];
 
   constructor(context) {
     this.#dom = createDom(context);
+  }
+
+  /**
+   * 선택자로 요소를 찾아 이벤트 리스너 등록
+   */
+  on(selector, eventType, handler) {
+    const element = this.#dom.querySelector(selector);
+
+    if (element) {
+      element.addEventListener(eventType, handler);
+      this.#eventListeners.push({
+        element,
+        eventType,
+        handler,
+      });
+    }
+
+    return this;
+  }
+
+  /**
+   * 직접 dom에 이벤트 리스너 등록
+   */
+  onElement(element, eventType, handler) {
+    if (element) {
+      element.addEventListener(eventType, handler);
+      this.#eventListeners.push({
+        element,
+        eventType,
+        handler,
+      });
+    }
+
+    return this;
   }
 
   getDom() {
