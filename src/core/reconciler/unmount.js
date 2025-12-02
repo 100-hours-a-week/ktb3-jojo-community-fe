40334b229d215621fca89544276f8b46934e4d9b
jieun0824 <suspen064@gmail.com>
2025-12-02 20:18:10 +0900

    fix: unmount 에 gc 위한 instance 참조 끊기

--- src/core/reconciler/unmount.js
+++ src/core/reconciler/unmount.js
@@ -5,8 +5,12 @@
  */
 
 export function unmount(parentDom, instance) {
+  console.log("unmount", parentDom, instance);
+  cleanupInstance(instance);
   parentDom.removeChild(instance.dom);
+}
 
+function cleanupInstance(instance) {
   //cleanup 처리 (commit 후)
   instance.hooks.forEach((hook) => {
     if (hook.tag === "effect") {
@@ -14,9 +18,8 @@ export function unmount(parentDom, instance) {
     }
   });
 
-  instance.childInstances?.forEach((hook) => {
-    if (hook.tag === "effect") {
-      hook.cleanup?.();
-    }
+  instance.childInstances?.forEach((instance) => {
+    cleanupInstance(instance);
   });
+  instance = null;
 }
